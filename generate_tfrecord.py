44af2b209bb0f84782e3d3895a092d467ac4ceea
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2020-06-29 15:28:38 +0200

    Update due to tensorflow 2.0

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 from PIL import Image
 from object_detection.utils import dataset_util
